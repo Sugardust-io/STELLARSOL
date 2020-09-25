595776a763f4291d90f3fff81c27f87373dbdf0a
Sugardust-io <jwule2262@gmail.com>
2020-09-25 16:12:24 -0400

    Data-viewer functionality finished. Working as intended.

--- collector_curses.py
+++ collector_curses.py
@@ -6,10 +6,11 @@ selected_option = ""
 
 
 # Main-menu.
-def mainMenu(root, current_row, h, w):
+def mainMenu(menu_win, current_row):
+    menu_win.clear()
+
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-    menu_win = curses.newwin(h - 1, w // 4, 1, 1)
     menu_win.border()
 
     menu_h, menu_w = menu_win.getmaxyx()
@@ -35,8 +36,9 @@ def mainMenu(root, current_row, h, w):
 
 
 # Data-entry menu.
-def dataMenu(root, h, w):
-    data_win = curses.newwin(h - 1, w // 2 + w // 4, 1, w // 4 + 1)
+def dataMenu(data_win):
+    data_win.clear()
+
     data_win.border()
 
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -77,7 +79,7 @@ def dataMenu(root, h, w):
 
     # Info-confirmation prompt.
     data_win.addstr(data_h // 2 - 5, 2, "Is this information correct? Y/N")
-    data_win.addstr(data_h // 2 - 4, 2, "Type 'Exit' to exit.")
+    data_win.addstr(data_h // 2 - 4, 2, "Type 'exit' to exit.")
     data_win.addstr(data_h // 2 - 2, 2, "Name of Object: " + obj_s)
     data_win.addstr(data_h // 2 - 1, 2, "RA/Dec: " + ra_s + " / " + dec_s) 
 
@@ -103,15 +105,139 @@ def dataMenu(root, h, w):
             data_win.addstr(data_h // 2 - 2, 2, "Data written to file.")
             break
         elif str(confirmation, 'utf-8') == "N":
-            dataMenu(root, h, w)
+            data_win.clear()
+            dataMenu(data_win)
             break
-        elif str(confirmation, 'utf-8') == "Exit":
-            exit()
+        elif str(confirmation, 'utf-8') == "exit":
+            break
         else:
             data_win.addstr(data_h // 2 + 2, 4, " ")
             continue
-
+    
+    data_win.clear()
     data_win.refresh()
+
+
+# Data-viewer.
+def dataViewer(viewer_win, root):
+    viewer_h, viewer_w = viewer_win.getmaxyx()
+
+    # Small sub-window for displaying short-info and user-input.
+    info_win = viewer_win.derwin(3, viewer_w, viewer_h - 3, 0)
+
+    page_1 = ["Alpha Centari-A", "Alpha Centari-B", "Alpha Centari-C", "Bernard's Star",
+    "Luhman 16", "WISE 0855-0714", "Wolf 359"]
+
+    pages = [page_1]
+
+    selection_s = 0
+
+    while True:
+        # Draw selected list to the data-viewer window.
+        def drawLists(selection_s):
+            viewer_win.clear()
+
+            for idx, element in enumerate(pages[int(selection_s) - 1]):
+                y = 1 + idx
+                x = 2
+
+                viewer_win.addstr(y, x, element)
+
+            viewer_win.refresh()
+        
+
+        viewer_win.border()
+        info_win.border()
+
+        page_string = "Scroll pages by typing their page number."
+
+        viewer_win.addstr(1, viewer_w - 9, "Pages: " + str(len(pages)))
+        info_win.addstr(1, 3, " " * 20)
+        info_win.addstr(1, 1, ">")
+        info_win.addstr(1, viewer_w // 2 - len(page_string) // 2, page_string)
+        info_win.addstr(1, viewer_w - 16, "'exit' to quit.")
+        
+        viewer_win.refresh()
+
+        curses.echo()
+        selection = info_win.getstr(1, 3)   # User-input
+        curses.noecho()
+
+        selection_s = str(selection, 'utf-8')
+
+        if selection_s == "1":
+            drawLists(selection_s)
+        elif selection_s == "exit":
+            break 
+        elif selection_s not in page_1:
+            continue
+        elif selection_s in page_1:
+            # Selected-object the user wishes to view.
+            selected_obj = selection_s
+
+            with open("data.json", "r") as json_file:
+                file_contents = json_file.read()
+            
+            obj_data = json.loads(file_contents)
+
+            json_file.close()
+
+            viewer_win.clear()
+            viewer_win.border()
+            info_win.border()
+            info_win.addstr(1, 1, ">")
+            viewer_win.refresh()
+
+
+            # 'data.json' is read and the object has it's number of indexes counted.
+            # If the object has no collected data, refer user to text document for more info.
+            def indexPrompt():
+                if len(obj_data[selected_obj]) == 0:
+                    print()
+                    print("Object has no collected data.")
+                    print("Please see 'stellarsol.txt' for more information about this. Sorry!")
+                    exit()
+                else:
+                    viewer_win.addstr(1, 1, selected_obj+ " has " + str(len(obj_data[selected_obj])) + " entries.")
+
+
+            indexPrompt()
+            viewer_win.refresh()
+
+            # The user is prompted to select an entry until a valid entry is selected.
+            # Relevant data for the selected-object is spit out into the terminal.
+            while True:
+                viewer_win.addstr(viewer_h - 4, 1, "Select an entry to view")
+                viewer_win.refresh()
+
+                curses.echo()
+                entry_selection = info_win.getstr(1, 3)
+                curses.noecho()
+
+                entry_selection = str(entry_selection, 'utf-8')
+
+                if entry_selection.isnumeric():
+                    if int(entry_selection) <= len(obj_data[selected_obj]):
+                        viewer_win.clear()
+
+                        viewer_win.addstr(1, 1, "Positional data for " + selected_obj + " on "
+                        + obj_data[selected_obj][int(entry_selection) - 1]["date"])
+
+                        viewer_win.addstr(3, 1, "RA/Dec: " + obj_data[selected_obj][int(entry_selection) - 1]["ra"]
+                        + " / " + obj_data[selected_obj][int(entry_selection) - 1]["dec"])
+                        break
+                    else:
+                        indexPrompt()
+                        continue
+                else:
+                    info_win.addstr(1, 3, " " * 20)
+                    indexPrompt()
+                    continue
+
+        viewer_win.refresh()
+
+    viewer_win.clear()
+    viewer_win.refresh()
 
 
 def main(root):
@@ -119,9 +245,11 @@ def main(root):
 
     h, w = root.getmaxyx()
 
-    current_row = 0
+    menu_win = curses.newwin(h - 1, w // 4, 1, 1)
+    data_win = curses.newwin(h - 1, w // 2 + w // 4, 1, w // 4 + 1)
+    viewer_win = curses.newwin(h - 1, w // 2 + w // 4, 1, w // 4 + 1)
 
-    mainMenu(root, current_row, h, w)
+    current_row = 0
 
     while True:
         key = root.getch()
@@ -131,15 +259,16 @@ def main(root):
         elif key == curses.KEY_DOWN and current_row < 2:
             current_row += 1
         elif key == ord("e") and selected_option == "Enter New Object Data":
-            dataMenu(root, h, w)
+            dataMenu(data_win)
+        elif key == ord("e") and selected_option == "View Collected Data":
+            dataViewer(viewer_win, root)
         elif key == ord("e") and selected_option == "Exit":
             exit()
         elif key == ord("q"):
             break
 
-        mainMenu(root, current_row, h, w)
+        mainMenu(menu_win, current_row)
     
     root.refresh()
-
 
 curses.wrapper(main)
